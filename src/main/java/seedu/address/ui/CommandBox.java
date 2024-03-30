903f9604479ead0933c5397fb236ed8c3f0be87a
Patrick <pshin0001@gmail.com>
2024-03-31 01:30:33 +0800

    Add AutoCompleteTextBox
    Key changes:
    * Now the suggestions table shows below when user type
    * User can click on the suggestion

--- src/main/java/seedu/address/ui/CommandBox.java
+++ src/main/java/seedu/address/ui/CommandBox.java
@@ -4,10 +4,19 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Region;
+import seedu.address.logic.commands.AddCommand;
+import seedu.address.logic.commands.EditCommand;
+import seedu.address.logic.commands.DeleteCommand;
+import seedu.address.logic.commands.FindCommand;
+import seedu.address.logic.commands.ListCommand;
+import seedu.address.logic.commands.ExitCommand;
+import seedu.address.logic.commands.HelpCommand;
+import seedu.address.logic.commands.TagCommand;
 import seedu.address.logic.commands.CommandResult;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.logic.parser.exceptions.ParseException;
 import org.controlsfx.control.textfield.TextFields;
+
 
 /**
  * The UI component that is responsible for receiving user command inputs.
@@ -19,6 +28,17 @@ public class CommandBox extends UiPart<Region> {
 
     private final CommandExecutor commandExecutor;
 
+    private static final String[] COMMANDS = {
+            AddCommand.COMMAND_WORD,
+            EditCommand.COMMAND_WORD,
+            DeleteCommand.COMMAND_WORD,
+            FindCommand.COMMAND_WORD,
+            ListCommand.COMMAND_WORD,
+            ExitCommand.COMMAND_WORD,
+            HelpCommand.COMMAND_WORD,
+            TagCommand.COMMAND_WORD
+    };
+
     @FXML
     private TextField commandTextField;
 
@@ -29,9 +49,9 @@ public class CommandBox extends UiPart<Region> {
         super(FXML);
         this.commandExecutor = commandExecutor;
         // calls #setStyleToDefault() whenever there is a change to the text of the command box.
-        String commands[] = {"/add", "/edit", "/delete", "/list"};
 
-        TextFields.bindAutoCompletion(commandTextField, commands);
+
+        TextFields.bindAutoCompletion(commandTextField, COMMANDS);
     }
 
     /**
