1fc8896da39eb2121e12a35de5fadc622e8ab1f4
Patrick <pshin0001@gmail.com>
2024-04-01 18:59:48 +0800

    Fix the Autocomplete
    Key changes:
    * Transaction command is added to Autocomplete

--- src/main/java/seedu/address/ui/CommandBox.java
+++ src/main/java/seedu/address/ui/CommandBox.java
@@ -4,9 +4,23 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Region;
+import seedu.address.logic.commands.AddCommand;
+import seedu.address.logic.commands.EditCommand;
+import seedu.address.logic.commands.DeleteCommand;
+import seedu.address.logic.commands.FindCommand;
+import seedu.address.logic.commands.ListCommand;
+import seedu.address.logic.commands.ExitCommand;
+import seedu.address.logic.commands.HelpCommand;
+import seedu.address.logic.commands.TagCommand;
 import seedu.address.logic.commands.CommandResult;
+import seedu.address.logic.commands.TransactionCommand;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.logic.parser.exceptions.ParseException;
+import org.controlsfx.control.textfield.TextFields;
+import org.controlsfx.control.textfield.AutoCompletionBinding;
+import java.util.Objects;
+
+
 
 /**
  * The UI component that is responsible for receiving user command inputs.
@@ -17,6 +31,19 @@ public class CommandBox extends UiPart<Region> {
     private static final String FXML = "CommandBox.fxml";
 
     private final CommandExecutor commandExecutor;
+    private ResultDisplay resultDisplay;
+
+    private static final String[] COMMANDS = {
+            AddCommand.COMMAND_WORD,
+            EditCommand.COMMAND_WORD,
+            DeleteCommand.COMMAND_WORD,
+            FindCommand.COMMAND_WORD,
+            ListCommand.COMMAND_WORD,
+            ExitCommand.COMMAND_WORD,
+            HelpCommand.COMMAND_WORD,
+            TagCommand.COMMAND_WORD,
+            TransactionCommand.COMMAND_WORD
+    };
 
     @FXML
     private TextField commandTextField;
@@ -24,11 +51,28 @@ public class CommandBox extends UiPart<Region> {
     /**
      * Creates a {@code CommandBox} with the given {@code CommandExecutor}.
      */
-    public CommandBox(CommandExecutor commandExecutor) {
+    public CommandBox(CommandExecutor commandExecutor, ResultDisplay resultDisplay) {
         super(FXML);
         this.commandExecutor = commandExecutor;
-        // calls #setStyleToDefault() whenever there is a change to the text of the command box.
-        commandTextField.textProperty().addListener((unused1, unused2, unused3) -> setStyleToDefault());
+        this.resultDisplay = resultDisplay; // Store the ResultDisplay reference
+
+        AutoCompletionBinding<String> autoCompletionBinding = TextFields.bindAutoCompletion(commandTextField, COMMANDS);
+        autoCompletionBinding.setOnAutoCompleted(event -> {
+            String selectedCommand = event.getCompletion();
+            if (Objects.equals(selectedCommand, AddCommand.COMMAND_WORD)) {
+                this.resultDisplay.setFeedbackToUser("Follows:\n" + ":name NAME :phone PHONE :email EMAIL " +
+                        ":address ADDRESS :year YEAR_JOINED :tag TAG");
+            } else if (Objects.equals(selectedCommand, EditCommand.COMMAND_WORD)) {
+                this.resultDisplay.setFeedbackToUser("Follows:\n" + "ID :phone PHONE and/or :email EMAIL " +
+                        "and/or :tag TAG");
+            } else if (Objects.equals(selectedCommand, DeleteCommand.COMMAND_WORD)) {
+                this.resultDisplay.setFeedbackToUser("Follows:\n" + "ID");
+            } else if (Objects.equals(selectedCommand, FindCommand.COMMAND_WORD)) {
+                this.resultDisplay.setFeedbackToUser("Follows:\n" + ":id, :name, :phone, :email, :year, :tag");
+            } else if (Objects.equals(selectedCommand, TagCommand.COMMAND_WORD)) {
+                this.resultDisplay.setFeedbackToUser("Follows:\n" + ":ID :tag TAG");
+            }
+        });
     }
 
     /**
