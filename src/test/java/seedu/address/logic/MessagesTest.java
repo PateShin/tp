9a7993a70db45440734d47f4e2031ea9af6c74fe
Patrick <pshin0001@gmail.com>
2024-04-02 15:34:49 +0800

    Add more test cases
    Key changes:
    * File MessagesTest.java is created
    * Test cases added to MessagesTest and ModelManagerTest

--- src/test/java/seedu/address/logic/MessagesTest.java
+++ src/test/java/seedu/address/logic/MessagesTest.java
@@ -1,2 +1,23 @@
-package seedu.address.logic.parser;public class MessagesTest {
+package seedu.address.logic;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static seedu.address.testutil.TypicalTransactions.TRANSACTION_1;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.model.transaction.Transaction;
+
+public class MessagesTest {
+
+    @Test
+    public void format_typicalTransaction_returnsCorrectString() {
+        Transaction transaction = TRANSACTION_1;
+        String expected = "Transaction ID: " + transaction.getId()
+                + "; DateTime: " + transaction.getDateTime()
+                + "; Employee ID: " + transaction.getEmployeeId()
+                + "; Amount: " + transaction.getAmount()
+                + "; Description: " + transaction.getDescription();
+
+        assertEquals(expected, Messages.format(transaction));
+    }
 }
