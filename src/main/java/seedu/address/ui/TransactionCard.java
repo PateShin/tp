db26dd672c1736892a3634ad8d26089720fe6b1c
Ondre Tann <ondre.tannzxc@gmail.com>
2024-04-02 18:55:26 +0800

    Add TransactionCard, TransactionDetailsPanel

---
+++ src/main/java/seedu/address/ui/TransactionCard.java
@@ -0,0 +1,46 @@
+package seedu.address.ui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
+import seedu.address.model.transaction.Transaction;
+
+/**
+ * An UI component that displays information of a {@code Person}.
+ */
+public class TransactionCard extends UiPart<Region> {
+
+    private static final String FXML = "TransactionListCard.fxml";
+
+    /**
+     * Note: Certain keywords such as "location" and "resources" are reserved keywords in JavaFX.
+     * As a consequence, UI elements' variable names cannot be set to such keywords
+     * or an exception will be thrown by JavaFX during runtime.
+     *
+     * @see <a href="https://github.com/se-edu/addressbook-level4/issues/336">The issue on AddressBook level 4</a>
+     */
+
+    public final Transaction transaction;
+
+    @FXML
+    private Label description;
+    @FXML
+    private Label transactionId;
+    @FXML
+    private Label amount;
+    @FXML
+    private Label employeeId;
+
+    /**
+     * Creates a {@code PersonCode} with the given {@code Person} and index to display.
+     */
+    public TransactionCard(Transaction transaction, int displayedIndex) {
+        super(FXML);
+        this.transaction = transaction;
+        transactionId.setText(Long.toString(transaction.getId().value));
+        description.setText(transaction.getDescription().value);
+        amount.setText(Double.toString(transaction.getAmount().value));
+        employeeId.setText(Integer.toString(transaction.getEmployeeId().value));
+    }
+}
